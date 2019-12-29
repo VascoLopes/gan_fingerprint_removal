6a0d9f5ba3611747dc98f229fba14b42de1ced6b
João C. Neves <joaoneves@tomiworld.com>
2019-12-29 19:26:59 +0000

    Added scripts for paper experiments (CNN)
    The scripts for running the experiments for 1) controlled scenario; 2) in the wild; 3) gan fingerprint removal

--- test_pytorch_gpu.py
+++ test_pytorch_gpu.py
@@ -25,14 +25,15 @@ def test_model(model, dataloaders):
             # Get model outputs and calculate correct label
             outputs = model(inputs)
 
-            _, preds=torch.max(outputs, 1)
-            norm_preds = F.softmax(outputs).cpu().numpy()
-            for pred,label in zip(norm_preds,labels.cpu().numpy()):
-                preds_list.append(pred[1])
+            #_, preds=torch.max(outputs, 1)
+            #norm_preds = F.softmax(outputs).cpu().numpy()
+            preds = outputs
+            for pred,label in zip(preds.cpu().numpy(),labels.cpu().numpy()):
+                preds_list.append(pred[0])
                 labels_list.append(label)
 
         # statistics
-        running_corrects += torch.sum(preds == labels.data)
+        running_corrects += torch.sum((preds>0.5) == labels.data)
 
     acc = running_corrects.double() / len(dataloaders['test'].dataset)
 
@@ -44,10 +45,9 @@ def test_model(model, dataloaders):
 if __name__ == '__main__':
     ''' ----------------------------- PARAMS -----------------------------  '''
 
-    img_size = 64
     n_class = 2
-    data_dir = 'real2fake_downsize_{0}_100F_VF2_train_NFF_VF2_test'.format(img_size)
-    model_dir = "..\\exp2\\real2fake_100F_VF2_train_NFF_VF2_test"
+    model_dir = "F:\\gan_fingerprint_removal\\data\\real2fake_VF2_TPDNE"
+    data_dir = "F:\\gan_fingerprint_removal\\data\\real2fake_VF2_TPDNE"
     batch_size = 32
     mean = [0.5, 0.5, 0.5]
     std = [0.5, 0.5, 0.5]
@@ -56,10 +56,8 @@ if __name__ == '__main__':
     use_gpu = True
 
     if len(sys.argv) > 1:
-        img_size = int(sys.argv[1])
-        data_dir = sys.argv[2]
-        model_dir = sys.argv[3]
-
+        data_dir = sys.argv[1]
+        model_dir = sys.argv[2]
 
     ''' ----------------------------- LOAD DATA -----------------------------  '''
 
@@ -87,6 +85,7 @@ if __name__ == '__main__':
 
     fpr, tpr, _ = roc_curve(labels_list, scores_list)
     roc_auc = auc(fpr, tpr)
+    print(roc_auc)
 
     fpr = np.expand_dims(np.asarray(fpr),axis=1)
     tpr = np.expand_dims(np.asarray(tpr),axis=1)
@@ -113,4 +112,3 @@ if __name__ == '__main__':
     plt.show()
     '''
 
-    print(roc_auc)
